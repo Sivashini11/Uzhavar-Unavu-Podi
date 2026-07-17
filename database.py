389579a43e8e9d3761400a0d9de03f3383522519
Sivashini P <sivashini91@gmail.com>
2026-07-17 14:19:04 +0530

    Fixed customer_name column

--- database.py
+++ database.py
@@ -1,37 +1,10 @@
 import sqlite3
 
 conn = sqlite3.connect("database.db")
+cur = conn.cursor()
 
-cursor = conn.cursor()
+cur.execute("PRAGMA table_info(orders)")
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS orders(
-
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-
-    order_id TEXT,
-
-    customer_name TEXT,
-
-    phone TEXT,
-
-    address TEXT,
-
-    product TEXT,
-
-    weight TEXT,
-
-    quantity INTEGER,
-
-    amount INTEGER,
-
-    status TEXT
-
-)
-""")
-
-conn.commit()
+print(cur.fetchall())
 
 conn.close()
-
-print("Database Created Successfully")
