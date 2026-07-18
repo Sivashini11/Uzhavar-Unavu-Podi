8c6a1d81ade7a09b10634ac82d5891dc96f54217
Sivashini P <sivashini91@gmail.com>
2026-07-18 20:24:21 +0530

    Created PostgreSQL orders table

--- database.py
+++ database.py
@@ -1,10 +1,24 @@
-import sqlite3
+from db import get_db
 
-conn = sqlite3.connect("database.db")
+conn = get_db()
 cur = conn.cursor()
 
-cur.execute("PRAGMA table_info(orders)")
+cur.execute("""
+CREATE TABLE IF NOT EXISTS orders(
+    id SERIAL PRIMARY KEY,
+    order_id TEXT,
+    customer_name TEXT,
+    phone TEXT,
+    address TEXT,
+    product TEXT,
+    weight TEXT,
+    quantity INTEGER,
+    amount INTEGER,
+    status TEXT
+)
+""")
 
-print(cur.fetchall())
-
+conn.commit()
 conn.close()
+
+print("Orders table created successfully.")
