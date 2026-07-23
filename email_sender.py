d957e859fc949ae2a0b821a484e85552dfa8ab3b
Sivashini P <sivashini91@gmail.com>
2026-07-23 20:59:16 +0530

    Added Resend package

--- email_sender.py
+++ email_sender.py
@@ -1,75 +1,37 @@
-import smtplib
 import os
-from email.message import EmailMessage
+import resend
 
-EMAIL = os.getenv("EMAIL_USER")
-PASSWORD = os.getenv("EMAIL_PASSWORD")
+resend.api_key = os.environ["RESEND_API_KEY"]
 
 OWNER_EMAIL = "uzhavarunavupodi@gmail.com"
 
-
 def send_invoice_to_owner(invoice_path, order):
 
-    print("Inside send_invoice_to_owner()")
+    with open(invoice_path, "rb") as f:
+        pdf_data = f.read()
 
-    msg = EmailMessage()
-
-    msg["Subject"] = f"🛒 New Order - {order['order_id']}"
-    msg["From"] = EMAIL
-    msg["To"] = OWNER_EMAIL
-
-    msg.set_content(f"""
-🌿 Uzhavar Unavu Podi
-
+    resend.Emails.send({
+        "from": "onboarding@resend.dev",
+        "to": OWNER_EMAIL,
+        "subject": f"New Order - {order['order_id']}",
+        "text": f"""
 New Order Received
 
-Order ID:
-{order['order_id']}
+Order ID: {order['order_id']}
+Customer: {order['name']}
+Phone: {order['phone']}
+Address: {order['address']}
+Product: {order['product']}
+Weight: {order['weight']} g
+Quantity: {order['quantity']}
+Amount: ₹{order['amount']}
+""",
+        "attachments": [
+            {
+                "filename": f"{order['order_id']}.pdf",
+                "content": pdf_data
+            }
+        ]
+    })
 
-Customer:
-{order['name']}
-
-Phone:
-{order['phone']}
-
-Address:
-{order['address']}
-
-Product:
-{order['product']}
-
-Weight:
-{order['weight']}
-
-Quantity:
-{order['quantity']}
-
-Amount:
-₹{order['amount']}
-""")
-
-    with open(invoice_path, "rb") as f:
-        msg.add_attachment(
-            f.read(),
-            maintype="application",
-            subtype="pdf",
-            filename=os.path.basename(invoice_path)
-        )
-
-    print("Connecting to Gmail...")
-
-    try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, timeout=20) as smtp:
-
-            print("Connected")
-
-            smtp.login(EMAIL, PASSWORD)
-
-            print("Logged in")
-
-            smtp.send_message(msg)
-
-            print("✅ Email sent successfully!")
-
-    except Exception as e:
-        print("❌ SMTP ERROR:", e)
+    print("✅ Email sent successfully using Resend")
